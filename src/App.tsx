f4a8a8b0fd8f414128e72d46b85767c8c708feed
Yanks <charlie4284@gmail.com>
2020-01-12 23:46:41 +0800

    feat: Sidebar items

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Sidebar from './components/Sidebar';
+import Pushable from './components/Pushable';
+import NetworkPage from './containers/NetworkPage';
+import { ThemeProvider } from './hooks/themes';
+import { PytorchColors } from './assets/colors';
+import MenuItem from './components/Menu/MenuItem';
+
+const containerStyle = {
+  width: '100vw',
+  height: '100vh',
+  display: 'flex',
+};
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div style={containerStyle} className="App">
+      <ThemeProvider colors={PytorchColors}>
+        <Sidebar>
+          <img
+            src={
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcYqqpDwMcpYCwGZrNcRcPov-ttKCuOxrUdUr0FSypc_tEIOSTcw&s'
+            }
+            alt={'Logo'}
+            style={{
+              width: '3em',
+              height: '3em',
+              display: 'block',
+              padding: '.5em',
+              paddingTop: '1em',
+              marginLeft: '1em',
+            }}
+          />
+          <MenuItem name={'Image processor'} style={{ padding: '.5em' }} />
+          <MenuItem name={'Network builder'} style={{ padding: '.5em' }} />
+        </Sidebar>
+        <Pushable>
+          <NetworkPage />
+        </Pushable>
+      </ThemeProvider>
     </div>
   );
 };
